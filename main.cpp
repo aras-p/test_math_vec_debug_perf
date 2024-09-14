ef6a67c6ebf141e2729b77fd91f37a24ddc426aa
Aras Pranckevicius <aras@nesnausk.org>
2024-09-14 15:01:48 +0300

    Raw scalar test

--- main.cpp
+++ main.cpp
@@ -24,6 +24,11 @@
 #include "test_filter.h"
 #undef USE_NAMESPACE
 
+#define USE_NAMESPACE test_raw
+#define USE_RAW_SCALAR_FILTER
+#include "test_filter.h"
+#undef USE_NAMESPACE
+
 
 void test_do_assert_msg(const char* file, int line, const char* function, const char* msg)
 {
@@ -72,21 +77,25 @@ int main()
 	uint8_t* dst_image2 = new uint8_t[kSize * kSize * 4];
 	uint8_t* dst_image3 = new uint8_t[kSize * kSize * 4];
 	uint8_t* dst_image4 = new uint8_t[kSize * kSize * 4];
+	uint8_t* dst_image5 = new uint8_t[kSize * kSize * 4];
 	float dt1 = test_assert_unroll::filter_image(kSize, src_image, dst_image1);
 	float dt2 = test_noassert_unroll::filter_image(kSize, src_image, dst_image2);
 	float dt3 = test_assert_loop::filter_image(kSize, src_image, dst_image3);
 	float dt4 = test_noassert_loop::filter_image(kSize, src_image, dst_image4);
-	printf("Time taken: %.1f %.1f %.1f %.1f ms\n", dt1, dt2, dt3, dt4);
+	float dt5 = test_raw::filter_image(kSize, src_image, dst_image5);
+	printf("Time taken: %.1f %.1f %.1f %.1f %.1f ms\n", dt1, dt2, dt3, dt4, dt5);
 	WriteTga("out-input.tga", kSize, kSize, (const uint32_t*)src_image);
 	WriteTga("out-output1.tga", kSize, kSize, (const uint32_t*)dst_image1);
 	WriteTga("out-output2.tga", kSize, kSize, (const uint32_t*)dst_image2);
 	WriteTga("out-output3.tga", kSize, kSize, (const uint32_t*)dst_image3);
 	WriteTga("out-output4.tga", kSize, kSize, (const uint32_t*)dst_image4);
+	WriteTga("out-output5.tga", kSize, kSize, (const uint32_t*)dst_image5);
 	delete[] src_image;
 	delete[] dst_image1;
 	delete[] dst_image2;
 	delete[] dst_image3;
 	delete[] dst_image4;
+	delete[] dst_image5;
 
 	return 0;
 }
--- test_filter.h
+++ test_filter.h
@@ -7,22 +7,54 @@
 namespace USE_NAMESPACE {
 #endif
 
-inline float4 load_pixel(const uint8_t* src, int size, int x, int y)
-{
-	x &= size - 1;
-	y &= size - 1;
-	uchar4 bpix(src + (y * size + x) * 4);
-	float4 pix = float4(bpix) * (1.0f / 255.0f);
-	return pix;
-}
-
-inline void store_pixel(uint8_t* dst, int size, int x, int y, float4 pix)
-{
-	pix = math_max(pix, float4(0.0f));
-	pix = math_min(pix, float4(1.0f));
-	pix = math_round(pix * 255.0f);
-	((uchar4*)dst)[y * size + x] = uchar4(pix);
-}
+#ifndef USE_RAW_SCALAR_FILTER
+	inline float4 load_pixel(const uint8_t* src, int size, int x, int y)
+	{
+		x &= size - 1;
+		y &= size - 1;
+		uchar4 bpix(src + (y * size + x) * 4);
+		float4 pix = float4(bpix) * (1.0f / 255.0f);
+		return pix;
+	}
+	inline void store_pixel(uint8_t* dst, int size, int x, int y, float4 pix)
+	{
+		pix = math_max(pix, float4(0.0f));
+		pix = math_min(pix, float4(1.0f));
+		pix = math_round(pix * 255.0f);
+		((uchar4*)dst)[y * size + x] = uchar4(pix);
+	}
+#else
+	inline void load_pixel(const uint8_t* src, int size, int x, int y, float pix[4])
+	{
+		x &= size - 1;
+		y &= size - 1;
+		const uint8_t* ptr = src + (y * size + x) * 4;
+		pix[0] = ptr[0] * (1.0f / 255.0f);
+		pix[1] = ptr[1] * (1.0f / 255.0f);
+		pix[2] = ptr[2] * (1.0f / 255.0f);
+		pix[3] = ptr[3] * (1.0f / 255.0f);
+	}
+	inline void store_pixel(uint8_t* dst, int size, int x, int y, const float pix[4])
+	{
+		float r = std::max(pix[0], 0.0f);
+		float g = std::max(pix[1], 0.0f);
+		float b = std::max(pix[2], 0.0f);
+		float a = std::max(pix[3], 0.0f);
+		r = std::min(r, 1.0f);
+		g = std::min(g, 1.0f);
+		b = std::min(b, 1.0f);
+		a = std::min(a, 1.0f);
+		r = std::round(r * 255.0f);
+		g = std::round(g * 255.0f);
+		b = std::round(b * 255.0f);
+		a = std::round(a * 255.0f);
+		uint8_t* ptr = dst + (y * size + x) * 4;
+		ptr[0] = uint8_t(r);
+		ptr[1] = uint8_t(g);
+		ptr[2] = uint8_t(b);
+		ptr[3] = uint8_t(a);
+	}
+#endif
 
 float filter_image(int size, const uint8_t* src, uint8_t* dst)
 {
@@ -35,6 +67,7 @@ float filter_image(int size, const uint8_t* src, uint8_t* dst)
 	{
 		for (int x = 0; x < size; x++)
 		{
+#ifndef USE_RAW_SCALAR_FILTER
 			float4 pix(0.0f);
 			float4 tint(x * inv_size, y * inv_size, 1.0f - x * inv_size, 1.0f);
 			for (int by = y - kFilter; by <= y + kFilter; by++)
@@ -48,6 +81,32 @@ float filter_image(int size, const uint8_t* src, uint8_t* dst)
 			}
 			pix *= 1.0f / ((kFilter * 2 + 1) * (kFilter * 2 + 1));
 			store_pixel(dst, size, x, y, pix);
+#else
+			float pix[4] = { 0,0,0,0 };
+			float tint[4] = { x * inv_size, y * inv_size, 1.0f - x * inv_size, 1.0f };
+			for (int by = y - kFilter; by <= y + kFilter; by++)
+			{
+				for (int bx = x - kFilter; bx <= x + kFilter; bx++)
+				{
+					float sample[4];
+					load_pixel(src, size, bx, by, sample);
+					sample[0] = sample[0] * (1.0f - blend) + tint[0] * blend;
+					sample[1] = sample[1] * (1.0f - blend) + tint[1] * blend;
+					sample[2] = sample[2] * (1.0f - blend) + tint[2] * blend;
+					sample[3] = sample[3] * (1.0f - blend) + tint[3] * blend;
+					pix[0] += sample[0];
+					pix[1] += sample[1];
+					pix[2] += sample[2];
+					pix[3] += sample[3];
+				}
+			}
+			float scale = 1.0f / ((kFilter * 2 + 1) * (kFilter * 2 + 1));
+			pix[0] *= scale;
+			pix[1] *= scale;
+			pix[2] *= scale;
+			pix[3] *= scale;
+			store_pixel(dst, size, x, y, pix);
+#endif
 		}
 	}
 	auto t1 = std::chrono::high_resolution_clock::now();
